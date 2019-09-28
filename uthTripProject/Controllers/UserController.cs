827f5f42563126d0d9248c96a9064d59c5006b28
Ksenia Klakovych <kklakovych@gmail.com>
2019-09-28 19:00:08 +0300

    Commit changes to local branch.

---
+++ uthTripProject/Controllers/UserController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using uthTripProject.Models;
+
+namespace uthTripProject.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult AddOrEdit(int id=0)
+        {
+            User userModel = new User();
+            return View(userModel);
+        }
+    }
+}
---
+++ uthTripProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(uthTripProject.Startup))]
+namespace uthTripProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
