41d1298ad9628af34b4b0873b5cca7106b63f31f
ksenyaklakovych <kklakovych@gmail.com>
2019-09-30 15:55:16 +0300

    Ksbranch (#22)
    * Changed displaying labels
    
    * Some changes

--- uthTripProject/Models/User.cs
+++ uthTripProject/Models/User.cs
@@ -13,7 +13,8 @@ namespace uthTripProject.Models
     using System.Collections.Generic;
     using System.ComponentModel;
     using System.ComponentModel.DataAnnotations;
-    
+    using System.ComponentModel.DataAnnotations.Schema;
+
     public partial class User
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
@@ -24,21 +25,30 @@ namespace uthTripProject.Models
             this.Rights = new HashSet<Right>();
             this.Trips = new HashSet<Trip>();
         }
-    
+
+        //[Key]
+        //[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int User_ID { get; set; }
 
+        [StringLength(30, MinimumLength = 3, ErrorMessage = "First name must have at least 3 chars.")]
+        [DisplayName("First name")]
         [Required(ErrorMessage ="This field is required.")]
         public string First_Name { get; set; }
 
+        [StringLength(30, MinimumLength = 3, ErrorMessage = "Last name must have at least 3 chars.")]
+        [DisplayName("Last name")]
         [Required(ErrorMessage = "This field is required.")]
         public string Last_Name { get; set; }
 
+        [DataType(DataType.EmailAddress)]
         [Required(ErrorMessage = "This field is required.")]
         public string Email { get; set; }
 
+        [StringLength(30, MinimumLength = 4, ErrorMessage = "User name must have at least 3 chars.")]
         [Required(ErrorMessage = "This field is required.")]
         public string Username { get; set; }
 
+        [StringLength(30, MinimumLength = 5, ErrorMessage = "Password must have at least 3 char.")]
         [DataType(DataType.Password)]
         public string Password { get; set; }
 
@@ -47,13 +57,19 @@ namespace uthTripProject.Models
         [Compare("Password")]
         public string ConfirmPassword { get; set; }
 
+        [DisplayName("Birthday date")]
         [DataType(DataType.Date)]
         [Required(ErrorMessage = "This field is required.")]
         public System.DateTime Birthday { get; set; }
 
+        [StringLength(30, MinimumLength = 5, ErrorMessage = "Photo URL must have at least 3 char.")]
+        [DisplayName("Your photo URL")]
         [Required(ErrorMessage = "This field is required.")]
         public string Photo_Url { get; set; }
 
+        [StringLength(30, MinimumLength = 10, ErrorMessage = "Info must have at least 3 char.")]
+        [DisplayName("Some information about you")]
+        [DataType(DataType.MultilineText)]
         [Required(ErrorMessage = "This field is required.")]
         public string Info { get; set; }
     
