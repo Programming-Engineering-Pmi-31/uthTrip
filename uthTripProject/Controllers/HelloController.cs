85581a13fdc7856a230d2699c664312990e75ee8
ksenyaklakovych <kklakovych@gmail.com>
2019-11-04 09:35:44 +0200

    Add new page: Hello (#59)

---
+++ uthTripProject/Controllers/HelloController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace uthTripProject.Controllers
+{
+    public class HelloController : Controller
+    {
+        // GET: Hello
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+        public ActionResult HelloPage()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult HelloPageAction(int a)
+        {
+            return RedirectToAction("Login");
+        }
+    }
+}
+
+
+
+//namespace uthTripProject.Controllers
+//{
+//    public class HelloController : Controller
+//    {
+
+
+//        public HelloController()
+//        {
+
+
+//        }
+
+//        public ActionResult Index()
+//        {
+//            return View();
+//        }
+
+//        [HttpGet]
+//        public ActionResult Hello()
+//        {
+//            return View();
+//        }
+
+//        [HttpPost]
+//        public ActionResult Hello(TripViewModel tripModel)
+//        {
+//            return RedirectToAction("Login");
+//        }
+
+//    }
+//}
