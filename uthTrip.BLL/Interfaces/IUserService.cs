5d149246b7a6f36cd1fe97d23f82f03afeaad3b8
Nadia Padalka <35704032+nadiapadalka@users.noreply.github.com>
2019-11-24 19:19:22 +0200

    Nadibranch (#89)
    * added unit tests to userservice
    
    Created tests using xunit and moq
    
    * added unit tests for userservice
    
    * added unit tests for userservice
    
    * Revert "added unit tests for userservice"
    
    This reverts commit 1f8c6ecabae0659b399dce7bb1a55d77d92c279a.
    
    * reverting
    
    * reverting

--- uthTrip.BLL/Interfaces/IUserService.cs
+++ uthTrip.BLL/Interfaces/IUserService.cs
@@ -12,14 +12,9 @@ namespace UthTrip.BLL.Interfaces
     {
         void CreateUser(UserDTO userDto);
         UserDTO GetById(int? id);
-
-         UserDTO GetByUsernamePassword(string username, string password);
-
-
         UserDTO Get(int userAccountId);
-        string GetFirstName(int userAccountId);
-
         UserDTO GetByUsernamePassword(string username, string password);
+        string GetFirstName(int userAccountId);
         IEnumerable<UserDTO> GetAll();
         void Dispose(int id);
         int FindMaxId();
--- uthTrip.BLL/Services/UserService.cs
+++ uthTrip.BLL/Services/UserService.cs
@@ -6,6 +6,9 @@ using uthTrip.DAL.Interfaces;
 using uthTrip.BLL.Infrastructure;
 using uthTrip.BLL.Interfaces;
 using System.Collections.Generic;
+
+
+
 using System.Linq;
 using uthTrip.DAL.EF;
 
@@ -49,6 +52,70 @@ namespace UthTrip.BLL.Services
             Database.Users.Create(user);
             Database.Save();
         }
+
+        //public int Authenticate(string username, string password)
+        //{
+        //    if (string.IsNullOrEmpty(username))
+        //    {
+        //        throw new Exception("Username is empty.");
+        //    }
+        //    else if (string.IsNullOrEmpty(password))
+        //    {
+        //        throw new Exception("Password is empty.");
+        //    }
+
+        //    var user = Database.Users.Find(u => u.Username == username).SingleOrDefault();
+        //    if (user == null)
+        //    {
+        //        throw new Exception("User with current name does not exist.");
+        //    }
+        //    else if (!VerifyHash(password, user.Hash))
+        //    {
+        //        throw new Exception("Invalid password.");
+        //    }
+
+        //    return user.Id;
+        //}
+        
+        public string GetFirstName(int userAccountId)
+        {
+            var result = GetAllUsers()
+                .Where(x => x.User_ID == userAccountId)
+                .Select(x => x.First_Name)
+                .FirstOrDefault();
+
+            return result;
+        }
+        public UserDTO Get(int userAccountId)
+        {
+            var userAccount = GetAllUsers()
+                .FirstOrDefault(x => x.User_ID == userAccountId);
+
+            return userAccount;
+        }
+        //public int Authenticate(string username, string password)
+        //{
+        //    if (string.IsNullOrEmpty(username))
+        //    {
+        //        throw new Exception("Username is empty.");
+        //    }
+        //    else if (string.IsNullOrEmpty(password))
+        //    {
+        //        throw new Exception("Password is empty.");
+        //    }
+
+        //    var user = Database.Users.Find(u => u.Username == username).SingleOrDefault();
+        //    if (user == null)
+        //    {
+        //        throw new Exception("User with current name does not exist.");
+        //    }
+        //    else if (!VerifyHash(password, user.Hash))
+        //    {
+        //        throw new Exception("Invalid password.");
+        //    }
+
+        //    return user.Id;
+        //}
 
 
 
@@ -101,6 +168,44 @@ namespace UthTrip.BLL.Services
                 return null;
             }
         }
+
+            private List<UserDTO> GetAllUsers()
+            {
+            DateTime somedate = new DateTime(2000, 07, 21);
+            return new List<UserDTO>()
+            {
+                    
+
+            
+            new UserDTO
+                {
+                    User_ID = 123,
+                    First_Name = "Simon",
+                    Last_Name = "Gilbert",
+                    Username = "simongilbert",
+                    Email= "simongilbert@com",
+                    Password = "1111",
+                    Birthday = somedate,
+                    Photo_Url = "www",
+                    Info = "some boy"
+
+                },
+                new UserDTO
+                {
+                    User_ID = 456,
+                    First_Name = "Alexander",
+                    Last_Name = "Hill",
+                    Username = "alexhill",
+                    Email= "alexhill@com",
+                    Password = "1111",
+                    Birthday = somedate,
+                    Photo_Url = "www",
+                    Info = "anpther boy"
+                },
+            };
+            }
+        }
+
 
             
         }
